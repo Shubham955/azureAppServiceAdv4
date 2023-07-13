7252f5be506e58aabb378dbb43173e6cba6e3b81
Shubham <shubham06@nagarro.com>
2023-07-14 01:02:22 +0530

    plugins added

---
+++ src/main/java/com/nagarro/trainee/advance/java/week/three/TShirtSearchWebApplication/dao/impl/ResetPasswordDaoImpl.java
@@ -0,0 +1,40 @@
+package com.nagarro.trainee.advance.java.week.three.TShirtSearchWebApplication.dao.impl;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+import com.nagarro.trainee.advance.java.week.three.TShirtSearchWebApplication.dao.ResetPasswordDao;
+import com.nagarro.trainee.advance.java.week.three.TShirtSearchWebApplication.model.UserAccount;
+
+@Component
+public class ResetPasswordDaoImpl implements ResetPasswordDao {
+
+	@Autowired
+	HibernateTemplate hibernateTemplate;
+
+	public int updateUserAccount(UserAccount userAccount) {
+//		session created
+		Session session=this.hibernateTemplate.getSessionFactory().openSession();
+		int recordsUpdated=-1;
+//			transaction started
+			session.beginTransaction();
+			
+			String hqlQuery="update UserAccount set password=:inputPassword where username=:inputUsername";
+			Query query=session.createQuery(hqlQuery);
+			query.setParameter("inputPassword", userAccount.getPassword());
+			query.setParameter("inputUsername", userAccount.getUsername());
+			
+			recordsUpdated=query.executeUpdate();
+
+//			transaction commited
+			session.getTransaction().commit();
+		
+//			session closed
+			session.close();
+		
+		return recordsUpdated;
+	}
+}
